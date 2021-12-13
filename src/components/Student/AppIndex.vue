49bf4d593bf590b0e69957f2a77a136297d3acad
codFish3 <2536109282@qq.com>
2021-12-13 16:17:15 +0800

    Update AppIndex.vue
    1

--- src/components/Student/AppIndex.vue
+++ src/components/Student/AppIndex.vue
@@ -1,36 +1,27 @@
 <template>
   <div class="block">
     <el-timeline>
+
       <el-timeline-item timestamp="2021/12/02" placement="top">
         <el-card>
           <h4>完成独立方案评价指标计算实验</h4>
-
-          <el-popover
-              placement="bottom"
-              title="哈哈哈哈哈"
-              :width="200"
-              trigger="click"
-              content="DDL于 2021/12/01 23:59"
-          >
-            <template #reference>
-              <el-button>surprise</el-button>
-            </template>
-          </el-popover>
-
-        </el-card>
-      </el-timeline-item>
-      <el-timeline-item timestamp="2021/12/02" placement="top">
-        <el-card>
-          <h4>完成软件规模度量实验</h4>
           <p>DDL于 2021/12/01 23:59</p>
         </el-card>
       </el-timeline-item>
+
+      <el-timeline-item timestamp="2021/12/02" placement="top">
+        <el-card>
+          <h4>完成软件规模度量实验</h4>
+        </el-card>
+      </el-timeline-item>
+
       <el-timeline-item timestamp="2021/12/02" placement="top">
         <el-card>
           <h4>完成财务分析实验</h4>
           <p>DDL于 2021/12/01 23:59</p>
         </el-card>
       </el-timeline-item>
+
     </el-timeline>
   </div>
 </template>
